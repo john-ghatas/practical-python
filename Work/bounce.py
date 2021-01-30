e14cfa62803255367834ba0cefbbc8bb2613431b
John Ghatas <johnghatas97@hotmail.com>
2021-01-31 00:47:09 +0100

    chore: Exercise 1.5 done

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
+#!/bin/python
+
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100.0
+count = 1
+
+while round(height, 4) > 0:
+    print(f'Bounce #{count} from {round(height, 4)}M')
+    count += 1
+    height *= 0.6
